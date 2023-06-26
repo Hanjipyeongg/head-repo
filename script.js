73c981ea20a1c4aeca71381baa38f44b3c2583c8
hanjipyeongg <ditohanan983@gmail.com>
2023-06-26 13:45:41 +0700

    Feat : add page testimonial

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-$('page-scroll').on('click', function(e) {
-
-  var tujuan = $(this).attr('href');
- 
-  var elemenTujuan = $(tujuan);
- 
-  $('html , body').animate({
-   scrollTop: elemenTujuan.offset().top - 50
+var cardContainer = document.getElementById("cardContainer");
+  var cardWrapper = document.getElementById("cardWrapper");
+  var scrollLeftButton = document.getElementById("scroll-left");
+  var scrollRightButton = document.getElementById("scroll-right");
+  // Scroll container to the left
+  scrollLeftButton.addEventListener("click", function() {
+    cardContainer.scrollBy({
+      left: -200, // Adjust the value based on scroll amount
+      behavior: "smooth" // Optional smooth scrolling effect
+    });
   });
- 
-  e.preventDefault();
- });
+
+  // Scroll container to the right
+  scrollRightButton.addEventListener("click", function() {
+    cardContainer.scrollBy({
+      left: 200, // Adjust the value based on scroll amount
+      behavior: "smooth" // Optional smooth scrolling effect
+    });
+  });
