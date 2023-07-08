52c581e292c6130f5b3689bcab12e9078c588adc
hanjipyeongg <ditohanan983@gmail.com>
2023-07-08 20:43:40 +0700

    feat : add swiper feature

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-var cardContainer = document.getElementById("cardContainer");
-  var cardWrapper = document.getElementById("cardWrapper");
-  var scrollLeftButton = document.getElementById("scroll-left");
-  var scrollRightButton = document.getElementById("scroll-right");
-  // Scroll container to the left
-  scrollLeftButton.addEventListener("click", function() {
-    cardContainer.scrollBy({
-      left: -200, // Adjust the value based on scroll amount
-      behavior: "smooth" // Optional smooth scrolling effect
-    });
-  });
-
-  // Scroll container to the right
-  scrollRightButton.addEventListener("click", function() {
-    cardContainer.scrollBy({
-      left: 200, // Adjust the value based on scroll amount
-      behavior: "smooth" // Optional smooth scrolling effect
-    });
+var swiper = new Swiper(".scroll-container", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+      nextEl: ".swipe-right",
+      prevEl: ".swipe-left",
+    },
+  
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
   });
